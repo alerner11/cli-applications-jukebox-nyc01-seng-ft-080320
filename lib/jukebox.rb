029cff009d6741f67d827049d692d216a640f54e
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-22 17:31:15 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,4 +1,8 @@
 def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- exist : exits this program"
 end
 
 def play
